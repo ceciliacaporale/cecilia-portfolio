ca2c7114221f96e6c2935cccbe06db2707414610
ceciliacaporale <cecicaporale16@gmail.com>
2025-03-16 17:48:57 -0300

    [comp][skills] - novo componente

---
+++ src/components/Skills/Skills.styles.ts
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+export const SkillsWrapper = styled.div`
+  width: 190px;
+  height: 238px;
+  background: white;
+  border-radius: 10px;
+  box-shadow: 0px 4px 10px rgba(219, 85, 85, 0.2);
+  position: absolute;
+  z-index: 99;
+  border: 1px solid #000;
+  box-shadow: 8px 8px 0 #7AEBC9;
+`;
+
+export const Header = styled.div`
+  height: 20px;
+  border-radius: 9px 9px 0 0;
+  display: flex;
+  align-items: center;
+  background-color:#FF86A4; 
+  border-bottom: 1px solid black;
+  padding: 0 12px;
+  position: relative;
+  cursor: grab;
+  user-select: none;
+
+  &:active {
+    cursor: grabbing;
+  }
+`;
+
+export const CloseButton = styled.button`
+  background: transparent;
+  border: none;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+export const Content = styled.div`
+  padding: 6px;
+  color: black;
+
+  .tag__name {
+  display: inline-block;
+  color: #fff;
+  font-size: 14px;
+  background-color:rgb(39, 34, 36);
+  padding: 3px 13px 5px;
+  border-radius: 10px;
+  margin: 6px 4px 4px 0;
+  position: relative;
+  text-transform: lowercase;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+}
+
+.tag__name:hover {
+  transform: scale(1.1);
+  background-color:rgb(221, 51, 122);
+}
+`;
+
+export const SmileWrapper = styled.div`
+  position: absolute;
+  top: -7px; 
+  right: 10px; 
+  z-index: 9999;
+`;
---
+++ src/components/Skills/Skills.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import useDraggable from "../../hooks/useDraggable"; 
+import { SkillsWrapper, Header, Content, SmileWrapper } from "./Skills.styles";
+import Cursor from "../../assets/cursor.png?url";
+
+interface SkillsProps {
+  title: string;
+}
+
+const Skills: React.FC<SkillsProps> = ({ title }) => {
+  const { position, handleMouseDown } = useDraggable(window.innerWidth - 400, 348);
+
+  return (
+    <SkillsWrapper style={{ left: position.x, top: position.y }}>
+       <SmileWrapper>
+        <img src={Cursor} alt="Smile" width="24" height="24" />
+      </SmileWrapper>
+    
+      <Header onMouseDown={handleMouseDown}>
+        {title}
+      </Header>
+      <Content>
+      <div className="card__tags">
+    <ul className="tag">
+      <li className="tag__name">JS</li>
+      <li className="tag__name">react.js</li>
+      <li className="tag__name">sass</li>
+      <li className="tag__name">typeScript</li>
+      <li className="tag__name">next</li>
+      <li className="tag__name">storybook</li>
+      <li className="tag__name">astro</li>
+      <li className="tag__name">styledComponents</li>
+      <li className="tag__name">css</li>
+      <li className="tag__name">strapi</li>
+      <li className="tag__name">figma</li>
+
+    </ul>
+    </div> 
+      </Content>
+    </SkillsWrapper>
+  );
+};
+
+export default Skills;
