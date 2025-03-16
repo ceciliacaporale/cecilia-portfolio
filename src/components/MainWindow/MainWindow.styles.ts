835bea1e6ffa4cc71b9acb1e4396aa921cb50fe7
ceciliacaporale <cecicaporale16@gmail.com>
2025-03-16 14:27:59 -0300

    [feat/[mainWindow] - melhorias componente

--- src/components/MainWindow/MainWindow.styles.ts
+++ src/components/MainWindow/MainWindow.styles.ts
@@ -2,27 +2,110 @@ import styled from "styled-components";
 
 export const MainWindowWrapper = styled.div`
   width: 90%;
-  height: 200px;
+  max-width: 80%px;
   background: white;
-  border-radius: 10px;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
+  border-radius: 12px;
   position: absolute;
   top: 50px;
   left: 50px;
   border: 1px solid #000;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+
+  @media (min-width: 1630px) {
+    width: 94%;
+  }
+`;
+
+export const WindowHeader = styled.div`
+  width: 100%;
+  height: 35px;
+  border-radius: 12px 12px 0 0;
+  display: flex;
+  align-items: center;
+  padding: 0 12px;
+  position: relative;
+`;
+
+export const WindowButtons = styled.div`
+  display: flex;
+  gap: 8px;
+
+  span {
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    display: inline-block;
+  }
+
+  .close {
+    background:rgb(252, 65, 174);
+  }
+  .minimize {
+    background: #ffbd2e;
+  }
+  .maximize {
+    background:rgb(102, 240, 123);
+  }
 `;
 
 export const Container = styled.div`
-  width: 100%;
-  position: absolute;
-  top: 50px;
-  left: 50px;
-  
+  flex: 1;
+  padding: 20px;
+  width: 80%;
+
+  h1 {
+    margin-bottom: 1rem;
+  }
+
   p {
     max-width: 60%;
+    margin-bottom: 1rem;
   }
 `;
 
 export const ContainerImage = styled.div`
-display: flex;
+  display: flex;
+  gap: 3rem;
+`;
+
+export const Tab = styled.div`
+  background: white;
+  border: 1px solid #000;
+  padding: 2px 8px;
+  border-radius: 8px 8px 0 0;
+  font-size: 14px;
+  top: 12px;
+  position: relative;
+  
+  .close-tab {
+    font-size: 10px;
+    font-weight: bold;
+    color: #000;
+    cursor: pointer;
+    padding: 0px;
+    transition: color 0.2s ease-in-out;
+    
+    &:hover {
+      color: #ff5555;
+    }
+  }
+
+  &:nth-child(1) {
+    background: #ffcccb;
+  }
+  &:nth-child(2) {
+    background: #add8e6; 
+  }
+  &:nth-child(3) {
+    background: #90ee90;
+  }
+`;
+export const TabsContainer = styled.div`
+  position: absolute;
+  top: 17px;
+  left: 60px;
+  display: flex;
+  z-index: 10;
 `;
--- src/components/MainWindow/MainWindow.tsx
+++ src/components/MainWindow/MainWindow.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MainWindowWrapper, Container, ContainerImage } from "./MainWindow.styles";
+import { MainWindowWrapper, Container, ContainerImage, TabsContainer, WindowHeader, WindowButtons, Tab } from "./MainWindow.styles";
 import Alvo from "../../assets/alvo.png"; 
 interface MainWindowProps {
   title?: string;
@@ -7,16 +7,36 @@ interface MainWindowProps {
 
 const MainWindow: React.FC<MainWindowProps> = () => {
   return (
+    <><TabsContainer>
+      <Tab>home  <span className="close-tab">✕</span></Tab>
+      <Tab>sobre mim <span className="close-tab">✕</span></Tab>
+      <Tab>blog <span className="close-tab">✕</span></Tab>
+    </TabsContainer>
     <MainWindowWrapper>
+        <WindowHeader>
+          <WindowButtons>
+            <span className="close"></span>
+            <span className="minimize"></span>
+            <span className="maximize"></span>
+          </WindowButtons>
+        </WindowHeader>
+
         <Container>
-        <h1>Hello World</h1>
-        <ContainerImage>
-        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo veniam in ipsum corrupti ratione fugit dicta consectetur voluptates tempora doloremque, aut praesentium eaque quisquam id labore rem, nam placeat. Expedita?</p>
-        <img src={Alvo as unknown as string} alt="Imagem testeee" />
-        </ContainerImage>
+          <h1>Hello World</h1>
+          <ContainerImage>
+          <p>
+            Sou desenvolvedora front-end e minha trajetória começou no início de
+            2022, quando iniciei a faculdade de Engenharia de Software. Logo
+            ingressei no mercado de trabalho, onde venho evoluindo constantemente,
+            sempre em busca de aprimoramento.
+            Ao longo desses anos, me especializei no desenvolvimento de
+            aplicações front-end escaláveis e responsivas, utilizando tecnologias
+          </p>
+            <img src={Alvo} alt="Imagem teste" />
+          </ContainerImage>
         </Container>
-    </MainWindowWrapper>
-  );
+      </MainWindowWrapper></>
+);
 };
 
 export default MainWindow;
