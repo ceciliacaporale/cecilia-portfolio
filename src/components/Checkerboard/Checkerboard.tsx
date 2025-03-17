f159e0844599f1c8e1e5d043c39d7b06d10a6d89
ceciliacaporale <cecicaporale16@gmail.com>
2025-03-17 20:54:06 -0300

    [comp][checkerboard] - novo componente

---
+++ src/components/Checkerboard/Checkerboard.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { CheckerboardWrapper } from "./Checkerboard.styles";
+
+const Checkerboard: React.FC = () => {
+  return <CheckerboardWrapper />;
+};
+
+export default Checkerboard;
