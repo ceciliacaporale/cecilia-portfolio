9e2890a23efb14407d45cd597247056e5a742e85
ceciliacaporale <cecicaporale16@gmail.com>
2025-03-23 10:46:15 -0300

    [feat][socialMedia] - add Instagram

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -4,6 +4,7 @@ import GitHubIcon from "../../assets/github.svg?url";
 import LinkedinIcon from "../../assets/linkedin.svg?url";
 import MediumIcon from "../../assets/medium.svg?url";
 import MailIcon from "../../assets/mail.svg?url";
+import InstagramIcon from "../../assets/instagram.svg?url";
 import Smile from "../../assets/smileface.svg?url";
 
 interface SocialMediaProps {
@@ -33,6 +34,12 @@ const SocialMedia: React.FC<SocialMediaProps> = ({ title, width, height,  iconSi
           <a href="https://www.linkedin.com/in/mariaceciliacaporale/" target="_blank" rel="noopener noreferrer">
             <img src={LinkedinIcon} alt="LinkedIn" width={iconSize} height={iconSize} />
             <Tooltip>LinkedIn</Tooltip>
+          </a>
+        </IconWrapper>
+        <IconWrapper>
+          <a href="https://medium.com/@cecicaporale16" target="_blank" rel="noopener noreferrer">
+            <img src={InstagramIcon} alt="Instagram" width={iconSize} height={iconSize} />
+            <Tooltip>Instagram</Tooltip>
           </a>
         </IconWrapper>
         <IconWrapper>
