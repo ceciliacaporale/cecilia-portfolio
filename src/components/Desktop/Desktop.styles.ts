b816a70ae591a058702799fe1497351a66e0af0f
ceciliacaporale <cecicaporale16@gmail.com>
2025-03-25 19:15:29 -0300

    [style] - add media query para altura da pagina

--- src/components/Desktop/Desktop.styles.ts
+++ src/components/Desktop/Desktop.styles.ts
@@ -1,14 +1,66 @@
 import styled from "styled-components";
 
+const heightStyles = `
+  height: 100vh;
+
+  @media (min-height: 758px) and (max-height: 829px) {
+    height: 110vh;
+  }
+
+  @media (min-height: 758px) and (max-height: 809px) {
+    height: 110vh;
+  }
+
+  @media (min-height: 681px) and (max-height: 757px) {
+    height: 123vh;
+  }
+
+ @media (min-height: 639px) and (max-height: 680px) {
+    height: 130vh;
+  }
+
+  @media (min-height: 639px) and (max-height: 664px) {
+    height: 130vh;
+  }
+
+  @media (min-height: 591px) and (max-height: 638px) {
+    height: 140vh;
+  }
+
+  @media (min-height: 500px) and (max-height: 590px) {
+    height: 150vh;
+  }
+
+  @media (min-height: 525px) and (max-height: 553px) {
+    height: 160vh;
+  }
+
+  @media (min-height: 489px) and (max-height: 524px) {
+    height: 170vh;
+  }
+
+  @media (min-height: 438px) and (max-height: 488px) {
+    height: 190vh;
+  }
+
+  @media (min-height: 344px) and (max-height: 437px) {
+    height: 240vh;
+  }
+
+  @media (max-height: 344px) {
+    height: 380vh;
+  }
+`;
+
 export const DesktopWrapper = styled.div`
   width: 100vw;
-  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: flex-end; 
   align-items: center;
   position: absolute;
 
+  ${heightStyles}
 `;
 
 export const ContentArea = styled.div`
@@ -36,7 +88,6 @@ export const ContentArea = styled.div`
   }
 
   @media (max-width: 1391px) {
-    width: 95%; 
-
+    width: 98%;
   }
 `;
