85775e329eb287e5ca7b9105d8e8e251cd8e70fe
ceciliacaporale <cecicaporale16@gmail.com>
2025-04-11 19:19:33 -0300

    [style] - deixando o responsiveLayout padronizado com Dekstop da home

--- src/components/ResponsiveLayout/ResponsiveLayout.styles.ts
+++ src/components/ResponsiveLayout/ResponsiveLayout.styles.ts
@@ -1,16 +1,69 @@
 import styled from "styled-components";
 
+const heightStyles = `
+  height: 100vh;
+
+  @media (min-height: 758px) and (max-height: 829px) {
+    height: 110vh;
+  }
+
+  @media (min-height: 758px) and (max-height: 809px) {
+    height: 110vh;
+  }
+
+  @media (min-height: 681px) and (max-height: 757px) {
+    height: 123vh;
+  }
+
+  @media (min-height: 639px) and (max-height: 680px) {
+    height: 130vh;
+  }
+
+  @media (min-height: 639px) and (max-height: 664px) {
+    height: 130vh;
+  }
+
+  @media (min-height: 591px) and (max-height: 638px) {
+    height: 140vh;
+  }
+
+  @media (min-height: 500px) and (max-height: 590px) {
+    height: 150vh;
+  }
+
+  @media (min-height: 525px) and (max-height: 553px) {
+    height: 160vh;
+  }
+
+  @media (min-height: 489px) and (max-height: 524px) {
+    height: 170vh;
+  }
+
+  @media (min-height: 438px) and (max-height: 488px) {
+    height: 190vh;
+  }
+
+  @media (min-height: 344px) and (max-height: 437px) {
+    height: 240vh;
+  }
+
+  @media (max-height: 344px) {
+    height: 380vh;
+  }
+  
+`;
 export const ProjectsWrapper = styled.div`
   width: 100vw;
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
-  height: 100vh;
   align-items: center;
   position: absolute;
 
-  @media (max-width: 795px) {
-    width: 100%;
+  ${heightStyles}
+
+  @media (min-height: 1346px) {
+    justify-content: center;
   }
 `;
 
@@ -27,21 +80,26 @@ export const ContainerMain = styled.div`
   position: relative;
   bottom: 0;
 
-  @media (max-width: 431px) {
-   height: 99%;
+  @media (min-height: 1120px) {
+    bottom: 196px;
+    height: 900px;
+    border-radius: 50px;
   }
 
   @media (max-width: 1900px) {
     max-width: 80rem;
   }
 
+  @media (min-width: 2380px) {
+    max-width: 1300px !important;
+  }
+
   @media (min-width: 1901px) {
-    max-width: 70%;
+    max-width: 1300px;
   }
 
   @media (min-width: 2063px) {
     max-width: 60%;
-    min-width: 60%;
   }
 
   @media (max-width: 1391px) {
@@ -51,16 +109,18 @@ export const ContainerMain = styled.div`
 
 export const ContentArea = styled.div`
   padding: 0 3rem;
+  height: 89%;
 
   @media (max-width: 795px) {
     width: 100%; 
-    border-radius: 2rem; 
     padding: 12px;
+    height: 99%;
   }
-  
-  @media (min-width: 796px) and (max-width: 1251px) {
+
+  @media (min-width: 796px) and (max-width: 1250px) {
     width: 100%;
     padding: 0 12px;
+    height: 96%;
   }
 `;
 
