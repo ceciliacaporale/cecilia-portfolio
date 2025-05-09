0971b8f22655c50594eb191a360c6a9c2785b2cb
ceciliacaporale <cecicaporale16@gmail.com>
2025-05-08 21:18:24 -0300

    fix: removendo alturas diferentes do mobile

--- src/layouts/ResponsiveLayout/ResponsiveLayout.styles.ts
+++ src/layouts/ResponsiveLayout/ResponsiveLayout.styles.ts
@@ -1,57 +1,5 @@
 import styled from "styled-components";
 
-const heightStyles = `
-  height: 100vh;
-
-  @media (min-height: 758px) and (max-height: 829px) {
-    height: 110vh;
-  }
-
-  @media (min-height: 758px) and (max-height: 809px) {
-    height: 110vh;
-  }
-
-  @media (min-height: 681px) and (max-height: 757px) {
-    height: 120vh;
-  }
-
-  @media (min-height: 639px) and (max-height: 680px) {
-    height: 130vh;
-  }
-
-  @media (min-height: 639px) and (max-height: 664px) {
-    height: 130vh;
-  }
-
-  @media (min-height: 591px) and (max-height: 638px) {
-    height: 140vh;
-  }
-
-  @media (min-height: 500px) and (max-height: 590px) {
-    height: 150vh;
-  }
-
-  @media (min-height: 525px) and (max-height: 553px) {
-    height: 160vh;
-  }
-
-  @media (min-height: 489px) and (max-height: 524px) {
-    height: 170vh;
-  }
-
-  @media (min-height: 438px) and (max-height: 488px) {
-    height: 190vh;
-  }
-
-  @media (min-height: 344px) and (max-height: 437px) {
-    height: 190vh;
-  }
-
-  @media (max-height: 344px) {
-    height: 200vh;
-  }
-  
-`;
 export const ProjectsWrapper = styled.div`
   width: 100vw;
   display: flex;
@@ -59,8 +7,6 @@ export const ProjectsWrapper = styled.div`
   justify-content: flex-end;
   align-items: center;
   position: absolute;
-
-  ${heightStyles}
 
   @media (min-height: 1346px) {
     justify-content: center;
