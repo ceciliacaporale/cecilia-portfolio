ddb1726a520f4b829b60ffae8e179a51e44b093a
ceciliacaporale <cecicaporale16@gmail.com>
2025-05-28 08:34:47 -0300

    style: ajuste de altura do desktop

--- src/layouts/Home/Desktop/Desktop.styles.ts
+++ src/layouts/Home/Desktop/Desktop.styles.ts
@@ -71,7 +71,7 @@ export const DesktopWrapper = styled.div`
 
 export const ContentArea = styled.div`
   margin-top: 4rem;
-  height: 99%;
+  height: 776px;;
   background-color: ${({ theme }) => theme.colors.white};
   border-radius: 50px 50px 0 0; 
   box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
