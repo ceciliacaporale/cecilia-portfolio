033519dd24cb951404fe3399115624cc2d505d7a
Maria Cecília <cecicaporale16@gmail.com>
2026-03-14 12:45:14 -0300

    chore: adiciona storybook na pagina de projetos

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,7 @@
 import ceciImg from "../assets/imagem-em-construcao.png?url";
 import boxShadow from "../assets/box-shadow.png?url";
 import PsicologaImg from "../assets/psicologa-laura.png?url";
+import Storybook from "../assets/storybook.png?url";
 
 export const projects = [
   {
@@ -11,17 +12,18 @@ export const projects = [
     link: "https://github.com/ceciliacaporale/shadow-lab"
   },
   {
+    title: "Design System",
+    description: "Documentação dos componentes do meu portfólio.",
+    imageSrc: Storybook,
+    technologies: ["React", "TypeScript", "Styled Components", "Storybook"],
+    link: "https://storybook.cecicaporale.com/"
+  },
+  {
     title: "Psicóloga Laura Lacerda",
     description: "Site institucional com foco na atuação clínica da psicóloga Laura Lacerda. Visual acolhedor, fácil navegação e responsivo.",
     imageSrc: PsicologaImg,
     technologies: ["Astro", "Styled Components", "JavaScript"],
     link: "https://www.psicologalauralacerda.com.br"
-  },
-  {
-    title: "Em Construção",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    imageSrc: ceciImg,
-    technologies: ["Next.js", "Tailwind", "JavaScript"],
   },
   {
     title: "Em Construção",
