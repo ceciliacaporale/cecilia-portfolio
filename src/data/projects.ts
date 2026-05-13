a499df4cfb388e91702d0b7c32a52cb5ce3ab5f6
Maria Cecília <cecicaporale16@gmail.com>
2026-05-13 16:05:20 -0300

    chore: altera texto para ingles

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,28 +7,28 @@ import Lucas from "../assets/lucasprof.png?url";
 export const projects = [
   {
     title: "Box Shadow Generator",
-    description: "Gere e copie sombras CSS personalizadas com visualização em tempo real.",
+    description: "Generate and copy custom CSS shadows with a real-time preview.",
     imageSrc: boxShadow,
     technologies: ["React", "TypeScript", "Vite", "Styled Components"],
     link: "https://github.com/ceciliacaporale/shadow-lab"
   },
   {
     title: "Design System",
-    description: "Documentação dos componentes do meu portfólio.",
+    description: "Documentation for my portfolio components.",
     imageSrc: Storybook,
     technologies: ["React", "TypeScript", "Styled Components", "Storybook"],
     link: "https://storybook.cecicaporale.com/"
   },
   {
-    title: "Psicóloga Laura Lacerda",
-    description: "Site institucional com foco na atuação clínica da psicóloga Laura Lacerda. Visual acolhedor, fácil navegação e responsivo.",
+    title: "Psychologist Laura Lacerda",
+    description: "Institutional website focused on psychologist Laura Lacerda's clinical practice. Features a welcoming design, easy navigation, and is fully responsive.",
     imageSrc: PsicologaImg,
     technologies: ["Astro", "Styled Components", "JavaScript"],
     link: "https://www.psicologalauralacerda.com.br"
   },
   {
     title: "Prof. Lucas Gomes",
-    description: "Site institucional com foco nas aulas particulares do Prof. Lucas Gomes. Visual moderno, navegação intuitiva e totalmente responsivo.",
+    description: "Institutional website focused on Prof. Lucas Gomes's private tutoring. Features a modern design, intuitive navigation, and is fully responsive.",
     imageSrc: Lucas,
     technologies: ["Next.js", "Tailwind", "TypeScript"],
     link: "https://www.lucasgomesprof.com.br/"
