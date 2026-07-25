aa33df92411276664bd9e6c1fbb32accc414c6a1
Maria Cecília <cecicaporale16@gmail.com>
2026-07-24 21:07:00 -0300

    feat: add PostThemeToggle for the static post page

---
+++ src/components/PostThemeToggle/PostThemeToggle.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ThemeWrapper from "../../ThemeWrapper";
+import ThemeToggleButton from "../ThemeToggleButton";
+
+const PostThemeToggle: React.FC = () => (
+  <ThemeWrapper>
+    <ThemeToggleButton iconSize={18} />
+  </ThemeWrapper>
+);
+
+export default PostThemeToggle;
